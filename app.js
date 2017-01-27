9a4c8ddabe2f0ca51f8fd1a48f8c65210529cd3a
Noelphilip1990 <noyo1000gt@gmail.com>
2017-01-27 13:02:25 +0530

    Add files via upload
    global variable

---
+++ app.js
@@ -0,0 +1,9 @@
+var i=1;
+function counter()
+{
+	return i++;
+}
+console.log("value: "+counter());
+console.log("value: "+counter());
+console.log("value: "+counter());
+console.log("value: "+counter());
